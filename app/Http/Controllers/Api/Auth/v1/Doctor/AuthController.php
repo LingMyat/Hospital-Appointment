e140b176da4efe0ca4e5c9cfbf4104eec62576fe
LingMyat223 <lingmyataung@outlook.com>
2023-02-08 17:06:39 +0630

    Doctor Api Start

--- app/Http/Controllers/Api/Auth/v1/Doctor/AuthController.php
+++ app/Http/Controllers/Api/Auth/v1/Doctor/AuthController.php
@@ -2,10 +2,85 @@
 
 namespace App\Http\Controllers\Api\Auth\v1\Doctor;
 
-use App\Http\Controllers\Controller;
+use App\Models\Doctor;
 use Illuminate\Http\Request;
+use App\Helper\ResponseHelper;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\DoctorResource;
+use App\Traits\MakeSlug;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    //
+    use MakeSlug;
+    public function login(Request $request)
+    {
+        $rules = [
+            'email_or_phone' => 'required',
+            'password' => 'required|string|min:6',
+        ];
+
+        if (filter_var($request->email_or_phone, FILTER_VALIDATE_EMAIL)) {
+            $rules['email_or_phone'] = 'required|email|exists:doctors,email';
+            $doctor = Doctor::where('email',$request->email_or_phone)->first();
+        } else {
+            $rules['email_or_phone'] = 'required|exists:doctors,phone';
+            $doctor = Doctor::where('phone',$request->email_or_phone)->first();
+        }
+        $request->validate($rules);
+
+        if (!$doctor || ! Hash::check($request->password, $doctor->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $doctor->createToken('Booking')->plainTextToken;
+        $data = [
+            'user'=>new DoctorResource($doctor),
+            'token'=>$token
+        ];
+        return ResponseHelper::success($data,'Login successful!');
+    }
+
+    public function register(Request $request)
+    {
+        $rules = [
+            'name' => 'required',
+            'password' => 'required|string|min:6',
+            'confirm_password' => 'required|same:password',
+        ];
+
+        if (filter_var($request->email_or_phone, FILTER_VALIDATE_EMAIL)) {
+            $rules['email_or_phone'] = 'required|email|unique:doctors,email';
+            $email = $request->email_or_phone;
+            $phone = Null;
+        } else {
+            $rules['email_or_phone'] = 'required|unique:doctors,phone';
+            $phone = $request->email_or_phone;
+            $email = Null;
+        }
+
+        $request->validate($rules);
+
+        try {
+            $doctor = Doctor::create([
+                'name'=>$request->name,
+                'slug'=>$this->makeSlug($request->name,'doctors'),
+                'email'=>$email,
+                'phone'=>$phone,
+                'password'=>$request->password
+            ]);
+            $token =  $doctor->createToken('Booking')->plainTextToken;
+            $data = [
+                'user' => new DoctorResource($doctor),
+                'token' => $token
+            ];
+
+            return ResponseHelper::success($data,'Successfully register account!');
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\v1\Doctor\AuthController as ApiDoctorAuthController;
 use App\Http\Controllers\Api\Auth\v1\Patient\AuthController as ApiPatientAuthController;
 use App\Http\Controllers\Api\v1\DiseaseContrller;
 use App\Http\Controllers\Api\v1\DoctorController;
 use App\Http\Controllers\Api\v1\NrcController;
 use App\Http\Controllers\Api\v1\Patient\AppointmentController as ApiPatientAppointmentController;
 use App\Http\Controllers\Api\v1\Patient\RoomController as ApiPatientRomController;
+use App\Http\Resources\DoctorResource;
 use App\Http\Resources\PatientProfileResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +24,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/profile', function (Request $request) {
+    if($request->user()->role=='doctor'){
+        return new DoctorResource($request->user());
+    }
     return new PatientProfileResource($request->user());
 });
 
@@ -34,7 +39,7 @@ Route::controller(ApiPatientAuthController::class)
         Route::middleware('auth:sanctum')
             ->group(function () {
                 Route::delete('/logout', 'logout');
-                Route::post('/update','update');
+                Route::post('/update', 'update');
             });
     });
 
@@ -62,16 +67,29 @@ Route::prefix('v1/patient')
         Route::controller(NrcController::class)
             ->prefix('nrc')
             ->group(function () {
-                Route::get('/','index');
-                Route::get('/code','nrcCode');
-                Route::get('/{nrc_code}/name','nrcNameMm');
+                Route::get('/', 'index');
+                Route::get('/code', 'nrcCode');
+                Route::get('/{nrc_code}/name', 'nrcNameMm');
             });
         Route::controller(ApiPatientRomController::class)
             ->prefix('rooms')
             ->group(function () {
-                Route::get('/','index');
-                Route::get('{id}/room-messages','roomMessages')->middleware('auth:sanctum');
-                Route::post('store-room-message','storeRoomMessage')->middleware('auth:sanctum');
-                Route::post('store-chat-image','storeChatImage')->middleware('auth:sanctum');
+                Route::get('/', 'index');
+                Route::get('{id}/room-messages', 'roomMessages')->middleware('auth:sanctum');
+                Route::post('store-room-message', 'storeRoomMessage')->middleware('auth:sanctum');
+                Route::post('store-chat-image', 'storeChatImage')->middleware('auth:sanctum');
             });
+    });
+
+
+Route::controller(ApiDoctorAuthController::class)
+    ->prefix('v1/doctor/auth')
+    ->group(function () {
+        Route::post('/login', 'login');
+        Route::post('/register', 'register');
+        // Route::middleware('auth:sanctum')
+        //     ->group(function () {
+        //         Route::delete('/logout', 'logout');
+        //         Route::post('/update', 'update');
+        //     });
     });
